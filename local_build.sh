e2129a1dcdc988ad9894a57e60b5a63aed5501da
hallstattmia <14307130159@fudan.edu.cn>
2022-03-07 19:34:43 +0800

    fix

--- local_build.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter Your File Path:"
-read prefix_path
-echo "${prefix_path}"
-cmake .
-make
-./split "${prefix_path}100x10x10/" "1" "${prefix_path}team-10/"
-./split "${prefix_path}100x10x10/" "2" "${prefix_path}team-10/"
-./merge "${prefix_path}team-10/"
-cp "${prefix_path}team-10/trade1/prev_price" "${prefix_path}team-10/trade_merge/prev_price" 
