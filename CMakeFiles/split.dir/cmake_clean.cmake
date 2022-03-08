cebbb101b28589f275777595bb8d609ff1041758
wxxlouisa <569937952@qq.com>
2022-03-08 22:10:12 +0800

    fix

---
+++ CMakeFiles/split.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/split.dir/split.cpp.o"
+  "CMakeFiles/split.dir/split.cpp.o.d"
+  "split"
+  "split.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/split.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
