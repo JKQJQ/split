32d30362618ec4c4a0f6e53f2d908a084087e96e
jiyanggong <jiyanggong@microsoft.com>
2022-03-17 22:36:53 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 ######## VARIABLES ################
-SPLIT_FILE=/data/100x1000x1000/
-TRADER_ID=1
-OUTPUT_FILE=/data/team-10/large/test${TRADER_ID}
-N=1000 #small: 10 medium: 100 large:1000
-
-# SPLIT_FILE=/data/100x10x10/
+# SPLIT_FILE=/data/100x1000x1000/
 # TRADER_ID=1
-# OUTPUT_FILE=/data/team-10/small/test${TRADER_ID}
-# N=10 #small: 10 medium: 100 large:1000
+# OUTPUT_FILE=/data/team-10/large/test${TRADER_ID}
+# N=1000 #small: 10 medium: 100 large:1000
+
+SPLIT_FILE=/data/100x10x10/
+TRADER_ID=1
+OUTPUT_FILE=/data/team-10/small/test${TRADER_ID}
+N=10 #small: 10 medium: 100 large:1000
 ######### END ###############
 
 ######## TEST_VARIABLES ################
-ANNOTHE_TRADER_ID=2
-ANOTHER_OUTPUT_FILE=/data/team-10/large/test${ANNOTHE_TRADER_ID}
-
 # ANNOTHE_TRADER_ID=2
-# ANOTHER_OUTPUT_FILE=/data/team-10/small/test${ANNOTHE_TRADER_ID}
+# ANOTHER_OUTPUT_FILE=/data/team-10/large/test${ANNOTHE_TRADER_ID}
+
+ANNOTHE_TRADER_ID=2
+ANOTHER_OUTPUT_FILE=/data/team-10/small/test${ANNOTHE_TRADER_ID}
 ######### END ###############
 cmake .
 make
